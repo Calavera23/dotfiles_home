d0baafeba92adb2dfe8cce55d969482567c6925a
Manny Calavera <32740059+Calavera23@users.noreply.github.com>
2020-12-23 14:06:53 +0200

    wow new commit

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -7,24 +7,32 @@ if status --is-login
   bass source /etc/profile
   bass source $HOME/.profile
 end
-set -gx EDITOR "code -w"
+set -gx EDITOR "subl -w"
 # https://bugs.launchpad.net/ubuntu/+source/kubuntu-meta/+bug/1886018
 set -gx GTK_USE_PORTAL 1
-# https://bugzilla.mozilla.org/show_bug.cgi?id=788319 
-set -gx MOZ_X11_EGL 1
-# wayland
-#set -gx MOZ_ENABLE_WAYLAND 1
+# ipega sdl bindings
+set -gx SDL_GAMECONTROLLERCONFIG "05000000491900000204000000000000,Ipega PG-9087S - Bluetooth Gamepad,a:b0,b:b1,y:b4,x:b3,start:b11,back:b10,leftstick:b13,rightstick:b14,leftshoulder:b6,rightshoulder:b7,dpup:h0.1,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:b8,righttrigger:b9,platform:Linux,"
+# wine env
+set -gx WINEESYNC 1
+set -gx WINEFSYNC 1
 
 # Shorter
 alias e 'code'
 alias exip 'curl -s https://ipecho.net/plain'
 alias fd 'fdfind'
 alias g 'git'
-alias letyshops 'firefox --profile $HOME/Documents/fxprofiles/letyshops'
+alias a 'source bin/activate.fish'
 alias vts 'echo vitetris --connect (exip):27015 && vitetris -listen 27015'
 
 function updatecleanup
   ubuntu-security-status --unavailable
   sudo apt autoremove
 end
-alias a 'source bin/activate.fish'
+
+# https://gitlab.com/freedesktop-sdk/freedesktop-sdk/-/issues/1133
+function freetypecleartype
+  fd -t f -H -I "libfreetype.so*" --search-path /var/lib/flatpak -x sudo cp -fv (realpath /usr/lib/x86_64-linux-gnu/libfreetype.so.6) {}
+  # fix `/app/extra/viber/Viber: error while loading shared libraries: libbrotlidec.so.1: cannot open shared object file: No such file or directory`
+  # will be fixed by https://github.com/flathub/com.viber.Viber/pull/27
+  sudo cp -v /var/lib/flatpak/runtime/org.kde.Platform/x86_64/5.15/active/files/lib/x86_64-linux-gnu/libbrotli* /var/lib/flatpak/runtime/org.freedesktop.Platform/x86_64/19.08/active/files/lib/x86_64-linux-gnu
+end
--- setup.sh
+++ setup.sh
@@ -3,23 +3,23 @@
 sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
 # wine
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-# can be disabled on distro upgrade
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ groovy main'
+# sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# repos
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
-sudo add-apt-repository -y ppa:mymedia/telegram
 sudo add-apt-repository -y ppa:kubuntu-ppa/backports
-# groovy is supported
-sudo add-apt-repository -y ppa:cdemu/ppa
 sudo add-apt-repository -y ppa:lutris-team/lutris
-sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
+#sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
 #### REPOS END
 
 sudo apt upgrade -y
 
 cd /tmp || exit 1
 # syncplay
-wget --content-disposition https://github.com/Syncplay/syncplay/releases/download/v1.6.6/syncplay_1.6.6.deb 'https://go.microsoft.com/fwlink/?LinkID=760868'
-sudo apt install -y ./code*.deb ./syncplay*.deb
+wget --content-disposition  https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://github.com/Syncplay/syncplay/releases/download/v1.6.6/syncplay_1.6.6.deb
+sudo apt install -y ./*.deb
 
 packages=(
   # optdeps
@@ -33,8 +33,8 @@ packages=(
   fd-find
   fish
   gamemode
-  gcdemu
   gimp
+  chntpw
   git-cola
   htop
   keepassxc
@@ -46,42 +46,39 @@ packages=(
   ppa-purge
   pulseaudio-modules-bt
   python3-{pip,venv}
-  python3.9 python3.9-venv
+  python3.9{,-venv}
   qbittorrent
   ripgrep
+  qdirstat
+  seahorse
   safeeyes
   shellcheck
   sirikali
   steam
   stow
   syncthing
-  telegram-desktop
   vitetris
   winehq-staging
   zeal
+  sublime-text
 )
 # wine and steam need this
 sudo dpkg --add-architecture i386
 # Install my packages
 sudo apt install --install-recommends -y "${packages[@]}"
 
-# snap
-sudo snap set system refresh.retain=2
-sudo snap set system refresh.timer=fri,9:00~21:00
-
-# vmware https://www.namhuy.net/227/enable-3d-hardware-graphics-acceleration-for-vmware-workstation-on-ubuntu
-echo 'mks.gl.allowBlacklistedDrivers = "TRUE"' >> "$HOME/.vmware/preferences"
-
 # flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub com.github.vladimiry.ElectronMail com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft org.freefilesync.FreeFileSync com.neatdecisions.Detwinner org.gtk.Gtk3theme.Breeze
+flatpak install -y flathub io.github.antimicrox.antimicrox org.telegram.desktop com.github.vladimiry.ElectronMail com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft org.freefilesync.FreeFileSync com.neatdecisions.Detwinner com.dropbox.Client org.gtk.Gtk3theme.Breeze
+sudo flatpak override org.telegram.desktop --filesystem=home
+sudo flatpak override com.discordapp.Discord --filesystem=home
+
+# python
+pip3 install -U git+https://github.com/simons-public/protonfixes@master protontricks
 
 # /etc/pam.d/sudo and /etc/pam.d/polkit-1
 sudo apt install -y fprintd libpam-fprintd
 sudo pam-auth-update
-
-# python
-pip3 install -U git+https://github.com/simons-public/protonfixes.git protontricks vdf
 
 # setup dofiles
 ../etc_cp/install.sh
@@ -90,16 +87,11 @@ pip3 install -U git+https://github.com/simons-public/protonfixes.git protontrick
 systemctl --user enable --now syncthing
 chsh -s /usr/bin/fish
 
-# meh
-# https://wiki.archlinux.org/index.php/XDG_Autostart
-# https://bugs.kde.org/show_bug.cgi?id=413053#c7
-mkdir "$HOME/.config/autostart"
-cd "$HOME/.config/autostart" || exit 1
-sed -e '$aHidden=True' /etc/xdg/autostart/org.kde.discover.notifier.desktop > org.kde.discover.notifier.desktop
-sed -e '$aHidden=True' /etc/xdg/autostart/gcdemu.desktop > gcdemu.desktop
+# fuck this shit
+sudo update-locale --reset
 
 # SBC HD
 # https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583 https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/227
-sudo sed -i 's|load-module module-bluetooth-discover|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa
+sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa
 
 mkdir -p $HOME/.local/bin
